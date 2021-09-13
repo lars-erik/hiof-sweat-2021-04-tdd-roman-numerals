7e43587d30d139a04d8e1ec653a7a651e5575d0d
Lars-Erik <lea@markedspartner.no>
2021-09-13 20:14:56 +0200

    Changed to if-statements to test while-loop theory. Failing test.

--- src/main/java/Numerals.java
+++ src/main/java/Numerals.java
@@ -3,16 +3,16 @@ public class Numerals {
     public static String toRoman(int number) {
         String result = "";
 
-        if (number == 3) {
-            result = "III";
+        if (number >= 1) {
+            result += "I";
         }
 
-        if (number == 2) {
-            result = "II";
+        if (number >= 1) {
+            result += "I";
         }
 
-        if (number == 1) {
-            result = "I";
+        if (number >= 1) {
+            result += "I";
         }
 
         return result;
