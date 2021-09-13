476bf502cd9defb92603f7b6c919f3605f0b4dd5
Lars-Erik <lea@markedspartner.no>
2021-09-13 19:25:41 +0200

    Compiling code with wrong result for I

--- src/test/java/Roman_Numerals.java
+++ src/test/java/Roman_Numerals.java
@@ -1,12 +1,12 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Roman_Numerals {
 
     @Test
     public void Converts_From_Decimal_To_Roman()
     {
-        fail("We don't have any code");
+        assertEquals("I", Numerals.toRoman(1));
     }
 }
