93d42605ad9cb838bdf4213bead3eeec3e19a44a
Lars-Erik <lea@markedspartner.no>
2021-09-13 19:59:40 +0200

    Test for 3 -> III passes

--- src/main/java/Numerals.java
+++ src/main/java/Numerals.java
@@ -1,6 +1,10 @@
 public class Numerals {
 
     public static String toRoman(int number) {
+        if (number == 3) {
+            return "III";
+        }
+
         if (number == 2) {
             return "II";
         }
