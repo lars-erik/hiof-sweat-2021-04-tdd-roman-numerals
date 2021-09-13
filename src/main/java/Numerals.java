a0dafe7ab84d91d34decd7b698ec665c9e65559e
Lars-Erik <lea@markedspartner.no>
2021-09-13 20:34:12 +0200

    Passing test for 4 -> IV

--- src/main/java/Numerals.java
+++ src/main/java/Numerals.java
@@ -3,6 +3,11 @@ public class Numerals {
     public static String toRoman(int number) {
         String result = "";
 
+        while (number >= 4) {
+            result += "IV";
+            number -= 4;
+        }
+
         while (number >= 1) {
             result += "I";
             number -= 1;
